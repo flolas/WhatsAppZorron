e5f399fa5b9113a2f76c8ae5e9d02cdca3adcd30
flolas <felipelolas@192.168.1.103>
2014-02-23 03:00:51 -0300

    Se agrego el modulo de Deportes, aun en pruebas.

--- Classes/Module/Deportes.php
+++ Classes/Module/Deportes.php
@@ -16,11 +16,14 @@ class Deportes extends \Library\WhatsApp\Module\Base {
 	 *
 	 * @var string
 	 */
-	protected $help = "Todavia no Implementado.
-					   !deportes <asistencias>\n
-					   !deportes <reservar> <rut> <#deporte>
-					   !deportes <cancelar> <rut> <#deporte>
-					   !deportes <ver> <rut>";
+	protected $help = "Primero debes utilizar este comando: !deportes key <usr> <pwd>\n
+					   \nEsto te dara una clave que debes guardar y utilizar para reservar deportes :)
+					   !deportes asistencias <key>\n
+					   !deportes ver <key>\n
+					   !deportes reservar <#deporte> <key>\n
+					   !deportes cancelar <#deporte> <key>\n
+					   !deportes renovar <#deporte> <key>\n
+					   !deportes estado <key>\n";
 
 
 	/**
@@ -37,21 +40,49 @@ class Deportes extends \Library\WhatsApp\Module\Base {
 	 */
 	public function command()
 	{
+		$args = $this->arguments;
+		if( stristr('asistencias',$args[0]) !== FALSE){
+			$r = 'Debes llevar 0 asistencias. Perrito, estamos de vacaciones! :)';
+			return;
+		}
+		elseif(stristr('key',$args[0]) !== FALSE) {
+			$data = "usr=".$args[1]."&pwd=".$args[2];
+			$ch = curl_init('http://api.salasuai.com/sports');
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+			'Content-Type: application/json',
+			'Content-Length: ' . strlen($data))
+			);
+			$r = curl_exec($ch);
+			return;
+		}
+		elseif(stristr('reservar',$args[0]) !== FALSE) {
+			$r = $this->fetch('http://api.salasuai.com/sports/reserve/sport/'.urlencode($args[1]).'/key/'.urlencode($args[2]));
+			return;
+		}
 		
-		$message = implode(' ', $this->arguments);
-		if( stristr('asistencias',$this->arguments[0]) !== FALSE){
-			$this->say('Debes llevar 0 asistencias. Perrito, estamos de vacaciones! :)');
+		elseif(stristr('cancelar',$args[0]) !== FALSE) {
+			$r = $this->fetch('http://api.salasuai.com/sports/cancel/sport/'.urlencode($args[1]).'/key/'.urlencode($args[2]));
 			return;
 		}
-		elseif(stristr('reservar',$this->arguments[0]) !== FALSE) {			$this->say('Todavia no implementado.');
-			return;}
-		elseif(stristr('cancelar',$this->arguments[0]) !== FALSE) {			$this->say('Todavia no implementado.');
-			return;}
-		elseif(stristr('ver',$this->arguments[0]) !== FALSE) {			$this->say('Todavia no implementado.');
-			return;}
+		elseif(stristr('renovar',$args[0]) !== FALSE) {
+			$r = $this->fetch('http://api.salasuai.com/sports/renovate/sport/'.urlencode($args[1]).'/key/'.urlencode($args[2]));
+			return;
+		}
+		elseif(stristr('ver',$args[0]) !== FALSE) {
+			$r = $this->fetch('http://api.salasuai.com/sports/get/key/'.urlencode($args[1]));
+			return;
+		}
+		elseif(stristr('estado',$args[0]) !== FALSE) {
+			$r = $this->fetch('http://api.salasuai.com/sports/status/key/'.urlencode($args[1]));
+			return;
+		}
 		else{
-			$this->say('Todavia no implementado.');
+			$r = $this->help;
 			return;
 		}
+		$this->say($r);
 		}
 	}
