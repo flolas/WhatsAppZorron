fec095efe15cc15ea25744f648543639715e88cf
flolas <felipelolas@192.168.1.103>
2014-02-23 05:01:25 -0300

    Arreglado errores

--- Classes/Module/Buses.php
+++ Classes/Module/Buses.php
@@ -48,10 +48,10 @@ class Buses extends \Library\WhatsApp\Module\Base {
 				if( stristr('proximo',$args[2]) !== FALSE && $args[1]){
 						$getJson = $this->fetch("http://api.salasuai.com/buses/location/".$l."/".$args[0]."/".$args[1]."/upcoming/1000");
 						$data=json_decode($getJson);
-						$this->say("http://api.salasuai.com/buses/location/".$l."/".$args[0]."/".$args[1]."/upcoming/1000");
 						$h="";
 						foreach($data as $prox){
-						$h="Un bus a las {$prox->static_time}. Tiempo de llegada:Ê{$prox->diff_time_min}";
+						$prox->diff_time_min =(string)$prox->diff_time_min;
+						$h="Un bus a las {$prox->static_time}. Tiempo de llegada:Ê{$prox->diff_time_min}\n";
 						}
 						$this->say("Buses\n".$h);
 				}
