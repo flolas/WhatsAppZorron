1c34b4ee5953aaaee0063272b7ae9cb7c3bb24a9
flolas <felipelolas@192.168.1.103>
2014-02-23 04:46:02 -0300

    Agregado arreglos a buses

--- Classes/Module/Buses.php
+++ Classes/Module/Buses.php
@@ -41,20 +41,28 @@ class Buses extends \Library\WhatsApp\Module\Base {
     public function command() {
 		$args = $this->arguments;
 		$l='santiago';
-			if(stristr((string)$args[2],'viña') !== FALSE || stristr((string)$args[2],'vina') !== FALSE) {
+			if(stristr((string)$args[2],'vina') !== FALSE) {
 				 $l='vina';
-				 $message = str_replace("vina", "", $message);
-				 $message = str_replace("viña", "", $message);
+				 $args[2] = str_replace("vina", "", $args[2]);
 			}
 				if( stristr('proximo',$args[1]) !== FALSE){
 						$getJson = $this->fetch("http://api.salasuai.com/buses/location/".$l."/".$args[0]."/upcoming/1000");
+						$data=json_decode($getJson);
+						$h="";
+						foreach($data as $prox){
+						$h="Un bus a las {$prox->static_time}. Tiempo de llegada:�{$prox->diff_time_min}";
+						}
+						$this->say("Buses\n".$h);
 				}
 				else{
 						$getJson = $this->fetch("http://api.salasuai.com/buses/location/".$l."/".$args[0]."/".$args[1]);
+						$data=json_decode($getJson);
+						$h="";
+						foreach($data as $hora){
+						$h="\n".$hora.$h;
+						}
+						$this->say("Buses\n".$h);
 				}
-				$data=json_decode($getJson);
-				echo print_r(json_decode($data));
-				$this->say($data);
 		
     }
 }
